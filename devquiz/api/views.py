8f2687c08b220ac951a39f996fe424a9db308610
hick-hpe <hpalermoemerick@gmail.com>
2025-05-31 17:36:46 -0300

    rm .vscode

--- devquiz/api/views.py
+++ devquiz/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import *
-from .serializers import *
+from .models import Quiz, Questao, Certificado
+from .serializers import DisciplinaSerializer, QuizSerializer, QuestaoSerializer, CertificadoPublicoSerializer
 
 
 @api_view(['GET', 'POST'])
